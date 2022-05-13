1730fe9eda766c08e845b4e3a80edb5903ce41d7
ayumiohno <ayumi0130ohno@gmail.com>
2022-05-13 14:01:09 +0900

    レポート追加

--- 01_matching/stable_matching.cpp
+++ 01_matching/stable_matching.cpp
@@ -40,14 +40,14 @@ void loop(std::vector<std::vector<int>>& rank_first, const std::vector<std::vect
 
 int main()
 {
-    std::vector<std::vector<int>> rank_first = {{4, 5, 2, 1, 0, 3},
+    std::vector<std::vector<int>> rank_second = {{4, 5, 2, 1, 0, 3},
         {0, 2, 5, 4, 1, 3},
         {4, 0, 3, 2, 1, 5},
         {4, 0, 5, 1, 3, 2},
         {0, 2, 1, 5, 4, 3},
 		{3, 2, 1, 0, 4, 5}};
 
-    std::vector<std::vector<int>> rank_second = {{2, 5, 3, 1, 4, 0},
+    std::vector<std::vector<int>> rank_first = {{2, 5, 3, 1, 4, 0},
         {3, 5, 0, 2, 1, 4},
         {3, 0, 5, 1, 2, 4},
         {5, 4, 1, 0, 3, 2},
